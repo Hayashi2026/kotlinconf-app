462bfb391c9b2b75956ceb59cb8da340f9d39563
xianchaohua <chaohua@guanghe.tv>
2018-10-25 18:29:46 +0800

    login 模块迁移

--- android/app/src/main/java/org/jetbrains/kotlinconf/login/LoginView.kt
+++ android/app/src/main/java/org/jetbrains/kotlinconf/login/LoginView.kt
@@ -1,4 +1,16 @@
 package org.jetbrains.kotlinconf.login
 
-class LoginView {
+class LoginView: LoginInterator.LoginPresenter {
+
+
+
+    override fun nextButtonClick(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideSoftKeyboard() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
--- common/src/commonMain/kotlin/org/jetbrains/kotlinconf/login/LoginInterator.kt
+++ common/src/commonMain/kotlin/org/jetbrains/kotlinconf/login/LoginInterator.kt
@@ -1,4 +1,50 @@
 package org.jetbrains.kotlinconf.login
 
 class LoginInterator {
+
+    lateinit var presenter: LoginPresenter
+
+    lateinit var loginService: LoginService
+
+    fun didBecomeActive() {
+        setNextButtonClick()
+        setCountryCodeClick()
+    }
+
+    /**
+     * 点击下一步按钮
+     */
+    private fun setNextButtonClick() {
+
+    }
+
+    /**
+     * 设置CountryCode点击事件
+     */
+    private fun setCountryCodeClick() {
+
+    }
+
+    /**
+     * 请求用户登录状态
+     */
+    private fun requestUserLoginState(phoneNumber: String) {
+
+    }
+
+    /**
+     * Presenter interface implemented by this RIB's view.
+     */
+    interface LoginPresenter {
+
+        fun nextButtonClick(): String
+
+        fun hideSoftKeyboard()
+
+    }
+
+}
+
+enum class LoginPageType {
+    PHONE, USERNAME_EMAIL
 }
